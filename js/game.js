8348e5536a25196579049360ddc97588f634ec21
unknown <kevinmalaverr@gmail.com>
2020-08-03 17:54:52 -0500

    randpm food

--- js/game.js
+++ js/game.js
@@ -18,12 +18,14 @@ class Game {
   tail;
   gameContext;
   time;
+  food;
 
   constructor() {
     this.canvas = document.getElementById("game");
     this.gameContext = this.canvas.getContext("2d");
     this.tail = new SnakeNode(16, 20);
     this.head = this.tail;
+    this.food = { x: 4, y: 6 };
 
     for (let i = 0; i < this.snakeLength; i++) {
       let nHead = new SnakeNode(this.head.x + 1, this.head.y);
@@ -56,8 +58,12 @@ class Game {
   }
 
   drawSnake() {
-    this.gameContext.fillStyle = "green";
     this.gameContext.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+    this.gameContext.fillStyle = "red";
+    this.gameContext.fillRect(this.food.x * 10, this.food.y * 10, 10, 10);
+
+    this.gameContext.fillStyle = "green";
 
     this.gameContext.fillRect(0, 0, 10, 400);
     this.gameContext.fillRect(390, 0, 10, 400);
@@ -73,25 +79,43 @@ class Game {
   }
 
   controlator = () => {
-    document.addEventListener("keyup", (event) => {
-      console.log();
+    const controller = (event) => {
+      let active;
       switch (event.keyCode) {
         case 38:
-          this.direction = "up";
+          if (this.direction != "down") {
+            this.direction = "up";
+            active = true;
+          }
           break;
         case 37:
-          this.direction = "left";
+          if (this.direction != "right") {
+            this.direction = "left";
+            active = true;
+          }
           break;
         case 39:
-          this.direction = "right";
+          if (this.direction != "left") {
+            this.direction = "right";
+            active = true;
+          }
           break;
         case 40:
-          this.direction = "down";
+          if (this.direction != "up") {
+            this.direction = "down";
+            active = true;
+          }
           break;
         default:
           break;
       }
-    });
+
+      if (active) {
+        document.removeEventListener("keydown", controller, false);
+      }
+    };
+
+    document.addEventListener("keydown", controller, false);
   };
 
   gameOver() {
@@ -108,15 +132,26 @@ class Game {
     if (colition) {
       this.gameOver();
     }
+
+    const eaten = this.head.x === this.food.x && this.head.y === this.food.y;
+    if (eaten) {
+      this.snakeLength++;
+      this.generateFood();
+    }
+  }
+
+  generateFood() {
+    this.food.x = Math.floor(Math.random() * 37) + 1;
+    this.food.y = Math.floor(Math.random() * 37) + 1;
   }
 
   init() {
-    this.controlator();
     this.time = setInterval(() => {
+      this.controlator();
       this.increaseDirection();
       this.calcColitions();
       this.drawSnake();
-    }, 1000);
+    }, 300);
   }
 }
 
